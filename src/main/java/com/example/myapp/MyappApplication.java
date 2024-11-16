664bc57af3cd82fd333e293d5035e296ac4f14b9
PaarXul <gian.parra96@gmail.com>
2024-11-16 10:40:58 -0300

    Se Configura el codigo para que mande un Mensaje y se deja el nombre del jar por defecto como myapp.jar

--- src/main/java/com/example/myapp/MyappApplication.java
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -1,13 +1,20 @@
 package com.example.myapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyappApplication {
+public class MyappApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+
+		System.out.println("Evaluacion DevOps");
+	}
 }
